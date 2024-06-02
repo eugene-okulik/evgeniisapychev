48a056b19900c2f0ba6a1229ae712e3978e4e109
Evgenii Sapychev <mraifrik@gmail.com>
2024-06-03 00:08:36 +0300

    homework_3_LINT_updetated zadanye_3.py and zadanye_4.py

--- homework/sapychev_evgenii/lesson_3/zadanye_3.py
+++ homework/sapychev_evgenii/lesson_3/zadanye_3.py
@@ -1,11 +1,7 @@
 import math
-
 x = int(input("введите число x"))
 y = int(input("введите число y"))
-
-peremenii = [x , y]
+peremenii = [x,y]
 n = (len(peremenii))
-
 print((x + y) / n)
 print(math.sqrt(x * y))
-
--- homework/sapychev_evgenii/lesson_3/zadanye_4.py
+++ homework/sapychev_evgenii/lesson_3/zadanye_4.py
@@ -1,5 +1,4 @@
 x = int(input("введите число x"))
 y = int(input("введите число y"))
-
 print(x**2 + y**2)
 print(x * y / 2)
