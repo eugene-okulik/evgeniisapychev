4de16f3e97eae44944e87570b4db288de0348771
Evgenii Sapychev <mraifrik@gmail.com>
2024-06-02 23:43:22 +0300

    homework_3_don

---
+++ homework/sapychev_evgenii/lesson_3/zadanye_3.py
@@ -0,0 +1,11 @@
+import math
+
+x = int(input("введите число x"))
+y = int(input("введите число y"))
+
+peremenii = [x , y]
+n = (len(peremenii))
+
+print((x + y) / n)
+print(math.sqrt(x * y))
+
---
+++ homework/sapychev_evgenii/lesson_3/zadanye_4.py
@@ -0,0 +1,5 @@
+x = int(input("введите число x"))
+y = int(input("введите число y"))
+
+print(x**2 + y**2)
+print(x * y / 2)
