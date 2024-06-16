f40e827c0271a2c22dcb0f6f21e57f7ebec17d92
Evgenii Sapychev <mraifrik@gmail.com>
2024-06-17 00:10:25 +0300

    homework_5_update_5.2_and_5.1_after_LINT1

--- homework/sapychev_evgenii/lesson_5/lesson_5.2.py
+++ homework/sapychev_evgenii/lesson_5/lesson_5.2.py
@@ -1,6 +1,6 @@
 a = 'результат операции: 42'
 b = 'результат операции: 514'
 c = 'результат работы программы: 9'
-print(int(a[a.index(": "):].replace(': ','')) + 10)
-print(int(b[b.index(": "):].replace(': ','')) + 10)
-print(int(c[c.index(": "):].replace(': ','')) + 10)
+print(int(a[a.index(": "):].replace(': ', '')) + 10)
+print(int(b[b.index(": "):].replace(': ', '')) + 10)
+print(int(c[c.index(": "):].replace(': ', '')) + 10)
