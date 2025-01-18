9c51bc09173c228bcda0d8fc5aaac8e6d857e395
EvgenySapychev <mraifrik@gmail.com>
2025-01-18 13:53:47 +0300

    lesson_8_done_lint_1

--- homework/sapychev_evgenii/lesson_8/lesson_8_1.py
+++ homework/sapychev_evgenii/lesson_8/lesson_8_1.py
@@ -2,7 +2,7 @@ import random
 
 salary = int(input('Какая у тебя зарплата?'))
 bonus = bool(random.getrandbits(1))
-if bonus == True:
+if bonus is True:
     salary = int(random.random() * 10000) + salary
     print(f'Я готов выдать тебе зарплату с премией, возьми {salary}(True)')
 else: 
