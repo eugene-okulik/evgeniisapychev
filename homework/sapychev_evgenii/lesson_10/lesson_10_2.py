a9c89b00ca024a77dc549b49e1528297acdaf77c
EvgenySapychev <mraifrik@gmail.com>
2025-01-28 23:20:57 +0300

    Sapychev_Lesson_10_done_lint_2

--- homework/sapychev_evgenii/lesson_10/lesson_10_2.py
+++ homework/sapychev_evgenii/lesson_10/lesson_10_2.py
@@ -1,11 +1,13 @@
 def main_func(func):
     def repeat_me(*args, **kwargs):
         count = kwargs.pop('count', 1)
+        
         def wrapper():
             for _ in range(count):
                 func(*args)
         return wrapper()
     return repeat_me
+
 
 @main_func
 def example(text):
