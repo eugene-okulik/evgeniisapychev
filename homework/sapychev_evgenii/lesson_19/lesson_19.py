158ae7ca85ab1628f03d74a5e2f9b6c052ea7ec3
EvgenySapychev <mraifrik@gmail.com>
2025-08-20 21:31:53 +0300

    Sapychev_Lesson_19_LINT1

--- homework/sapychev_evgenii/lesson_19/lesson_19.py
+++ homework/sapychev_evgenii/lesson_19/lesson_19.py
@@ -1,17 +1,16 @@
-import requests 
+import requests
 
 
 def create_obj():
     body = {
         "data": {'!json_111': '!one_1', '!json_211': '!two_2', '!json_311': 3},
-        "name": "Lesson_19_hello"
-        }
+        "name": "Lesson_19_hello"}
     headers = {'Content-Type': 'application/json'}
     response = requests.post('http://objapi.course.qa-practice.com/object', json=body, headers=headers).json()
-    assert response['data'] == {'!json_111': '!one_1', '!json_211': '!two_2', '!json_311': 3} , 'Data incorrect'
+    assert response['data'] == {'!json_111': '!one_1', '!json_211': '!two_2', '!json_311': 3}, 'Data incorrect'
     assert response['name'] == "Lesson_19_hello", 'Name incorrect'
     return response['id']
-    
+
 
 def clear_create_obj(create_id):
     requests.delete(f'http://objapi.course.qa-practice.com/object/{create_id}')
@@ -22,10 +21,10 @@ def put_obj():
     create_id = create_obj()
     body = {
         "data": {'json_111': 'one_1', 'json_211': 'two_2', 'json_311': 3},
-        "name": "Lesson_19_bye"
-        }
+        "name": "Lesson_19_bye"}
     headers = {'Content-Type': 'application/json'}
-    response = requests.put(f'http://objapi.course.qa-practice.com/object/{create_id}', json=body, headers=headers).json()
+    response = requests.put(f'http://objapi.course.qa-practice.com/object/{create_id}', json=body, 
+                            headers=headers).json()
     assert response['data'] == {'json_111': 'one_1', 'json_211': 'two_2', 'json_311': 3}
     assert response['name'] == "Lesson_19_bye", 'Name incorrect'
     clear_create_obj(create_id)
@@ -38,5 +37,8 @@ def patch_obj():
         'name': 'Lesson_19_path'
     }
     headers = {'Content-Type': 'application/json'}
-    response = requests.patch(f'http://objapi.course.qa-practice.com/object/{create_id}', json=body, headers=headers).json()
+    response = requests.patch(f'http://objapi.course.qa-practice.com/object/{create_id}', json=body,
+                               headers=headers).json()
+    assert response['data'] == {'patch_1': 'p1', 'patch=2': 'p2', 'patch_3': 3}
+    assert response['name'] == "Lesson_19_path", 'Name incorrect'
     clear_create_obj(create_id)
