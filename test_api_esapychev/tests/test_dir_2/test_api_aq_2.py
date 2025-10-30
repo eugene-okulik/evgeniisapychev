b761ee0c8863589e33572c6fc9f06cef24013fac
EvgenySapychev <mraifrik@gmail.com>
2025-10-31 01:31:58 +0300

    Sapychev_Evgenii_Lesson_22_Lint_2

--- test_api_esapychev/tests/test_dir_2/test_api_aq_2.py
+++ test_api_esapychev/tests/test_dir_2/test_api_aq_2.py
@@ -2,29 +2,24 @@ import pytest
 
 DATA = [
     {
-        "data": {"Column1": "Имя 1", "Column2": True, "Column3": 3},
-        "name": "neEvgeniiSapychev",
-    },
-    {
-        "data": {"Column1": "Имя 2", "Column2": False, "Column3": 3},
-        "name": "sovsemNeEvgeniiSapychev",
-    },
-    {ы
-        "data": {"Column1": "Имя 3", "Column2": True, "Column3": 3},
-        "name": "EvgeniiSapychev",
-    },
+        "data": {"Column1": "Изменил с помощью PUT", "Column2": True, "Column3": 2},
+        "name": "Изменил с помощью PUT",
+    }
+]
+
+PATCH_DATA = [
+    {"name": "Изменил с помощью PATCH"}
 ]
 
 
 @pytest.mark.parametrize("data", DATA)
-def test_create_obj(created_post_endpoint, data, delete_post_endpoint, start_srop, every_test):
-    created_post_endpoint.create_new_post(payload=data)
-    created_post_endpoint.check_data(data["data"])
-    created_post_endpoint.check_name(data["name"])
-    delete_post_endpoint.delete_post(created_post_endpoint.post_id)
-
-
-@pytest.mark.parametrize("data", DATA)
-def test_patch_obj(put_and_patch_endpoint, put_post_endpoint, data, every_test):
-    put_post_endpoint.put_post(put_and_patch_endpoint, data)
+def test_put_obj(created_and_deleted_post, put_post_endpoint, data):
+    put_post_endpoint.put_post(created_and_deleted_post, data)
     put_post_endpoint.check_name(data["name"])
+    put_post_endpoint.check_data(data["data"])
+
+
+@pytest.mark.parametrize("data", PATCH_DATA)
+def test_patch_obj(created_and_deleted_post, patch_post_endpoint, data):
+    patch_post_endpoint.patch_post(created_and_deleted_post, data)
+    patch_post_endpoint.check_name(data["name"])
